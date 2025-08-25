c63194b6dc8631a599b5003ec589e564b59d5d60
LucasIrvin <lucasgamerba@gmail.com>
2025-08-25 16:52:29 -0300

    Aula 2 Docstrings como comentários

--- aula1.py
+++ aula1.py
@@ -1,4 +1,14 @@
+"""
+Não é um comentario mais posso utilizar como um, o nome é docstring
+
+O interpretador do Python lê isso mais ignora 
+"""
+
+'''
+Usar para escrever suas notas
+'''
+
 # Permite escrever um comentário em uma unica linha
 print(123) # Na frente
 # Abaixo
-print(123)
+print(456)
