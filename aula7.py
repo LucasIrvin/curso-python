2f58b2ef6ed9019c7e56141dcd47475dba43960a
LucasIrvin <lucasgamerba@gmail.com>
2025-08-26 12:44:06 -0300

    Aula 7 Variaveis

---
+++ aula7.py
@@ -0,0 +1,13 @@
+# nome_completo = 'Lucas Irvin'
+# soma_dois_mais_dois = 2 + 2
+# int_um = int('5')
+
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Lucas'
+idade = 17
+maior_idade = idade >= 18
+print('Nome:', nome)
+print('idade:', idade)
+print('Essa pessoa é maior de idade?', maior_idade)
