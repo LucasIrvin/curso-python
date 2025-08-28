2353c3b62922abf77aa8497a6908e8487455f194
LucasIrvin <lucasgamerba@gmail.com>
2025-08-28 15:39:09 -0300

    Aula 27 Fatiamento de Strings e função len

---
+++ aula27.py
@@ -0,0 +1,13 @@
+"""
+Fatiamento de Strings
+
+012345678
+Olá mundo
+-987654321
+Fatiamento [i:f:p] [::] - I = Inicio, F = Fim, P = Passo é quantos caracteres ele vai pular
+Sempre que eu querer o final tem que pegar 1 a mais do que ta pedindo pois o indice final não é incluido
+Obs. : a função len retorna a qtd de caracteres da str
+"""
+
+variavel = 'Olá mundo'
+print(variavel[::-1])
