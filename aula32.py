22a66f1cb9ffa2a167afa6bafea7e2b474d994b5
LucasIrvin <lucasgamerba@gmail.com>
2025-08-29 15:44:42 -0300

    Aula 32 Exercicio 2

--- aula32.py
+++ aula32.py
@@ -3,16 +3,38 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-numero = input('Digite um número inteiro: ')
+# numero = input('Digite um número inteiro: ')
+
+# try:
+#     numero_int = int(numero)
+#     conta = numero_int % 2 == 0
+#     if conta is True:
+#         print('Este número é par')
+#     else:
+#         print('Este número é impar')
+# except:
+#     print('Você não digitou um número inteiro!')
+
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
 
 try:
-    numero_int = int(numero)
-    conta = numero_int % 2 == 0
-    if conta is True:
-        print('Este número é par')
-    else:
-        print('Este número é impar')
+    hora = input('Que horas são? ')
+    hora_int = int(hora)
 except:
-    print('Você não digitou um número inteiro!')
+    print('Digite um número inteiro!')
+
+if hora_int >= 0 and hora_int <= 11: 
+    print('Bom dia')
+elif hora_int >= 12 and hora_int <= 17:
+    print('Boa tarde')
+elif hora_int >= 18 and hora_int <= 23:
+    print('Boa noite')
+else:
+    print('Esse horário não existe')
+
 
 
