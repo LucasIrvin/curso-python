c8147f3a7f1cb057a360bbf6fe5161abb9cb96a7
LucasIrvin <lucasgamerba@gmail.com>
2025-08-29 11:41:22 -0300

    Aula 29 Introdução try except

--- aula28.py
+++ aula28.py
@@ -1,7 +1,7 @@
 
 nome = input('Digite seu nome: ')
 idade = input('Digite sua idade: ')
-
+ 
 if nome and idade != '':
     print(f'Seu nome é {nome}')
     print(f'Seu nome invertido é {nome[::-1]}')
