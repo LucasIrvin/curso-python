97739b126948a78998c2af5e7ff6a7cb18428acc
LucasIrvin <lucasgamerba@gmail.com>
2025-09-01 17:46:56 -0300

    Aula 39 Exercicio corrigido

--- aula39.py
+++ aula39.py
@@ -3,19 +3,15 @@ Iterando strings com while
 """
 
 #       012345678910
-nome = ' Lucas Irvin' # Iteráveis
-tamanho = 0
+nome = 'Lucas Irvin' # Iteráveis
 
-tamanho_nome = len(nome)
-print(nome)
-print(tamanho_nome)
+indice = 0
+novo_nome = ''
 
-print(nome[4])
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
 
-l1 = -1
-l2 = 0
-
-while l1 < tamanho_nome:
-    l1 += 1
-    l2 += 1 
-    print(f'{nome[l1:l2:]}', end="*")
+novo_nome += '*'
+print(novo_nome)
