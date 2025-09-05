3b542e1348f9b23b11193674b3f9a2759e3efcf6
LucasIrvin <lucasgamerba@gmail.com>
2025-09-05 10:50:51 -0300

    Aula 43 for, String interada com for

--- aula43.py
+++ aula43.py
@@ -1,9 +1,14 @@
+# texto = 'Python'
+
+# i = 0
+# tamanho_string = len(texto)
+
+# while i < tamanho_string:
+#     print(texto[i])
+
+#     i += 1
+
 texto = 'Python'
 
-i = 0
-tamanho_string = len(texto)
-
-while i < tamanho_string:
-    print(texto[i])
-
-    i += 1
+for letra in texto:
+    print(letra)
