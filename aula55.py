bae40620940fcfefa36f2a66210e338734264940
LucasIrvin <lucasgamerba@gmail.com>
2025-09-11 14:12:58 -0300

    Aula 55 Introdução ao empacotamento e desempacotamento

---
+++ aula55.py
@@ -0,0 +1,8 @@
+"""
+Introdução ao desempacotamento + tuples (tuplas)
+Qualquer interavel consigo fazer isso
+Utilizamos o '_' para indicar que não vamos usar
+"""
+
+_, _, nome, *_ = ['Maria', 'Helena', 'Luiz']
+print(nome)
