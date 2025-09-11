db3477ccee525529e006181cb2f5358d8c4aa636
LucasIrvin <lucasgamerba@gmail.com>
2025-09-11 14:36:47 -0300

    Aula 56 Tipo tuple (tuplas)

---
+++ aula56.py
@@ -0,0 +1,9 @@
+"""
+Tipo tupla - Uma lista imutável
+Quando não faz sentido eu alterar a lista eu crio uma Tuple
+"""
+nomes = ('Maria', 'Helena', 'Luiz')
+# nomes = tuple(nomes) Convertendo lista para tuple
+# nomes = list(nomes) Convertendo tuple para lista
+print(nomes[-1])
+print(nomes)
